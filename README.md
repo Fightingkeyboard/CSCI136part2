68df371139054bd50bf60f5c1b4b735a0aeedbf3
Fightingkeyboard <anthony.chen28@myhunter.cuny.edu>
2018-03-29 10:55:49 -0400

    read over & commented

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CSCI136part2
+## Forked by Anthony
+### Observations Here
+### Commented in code
+
+1. Has uses namespace std, MZ disapproves.
+2. File funcs.cpp consists of removeLeadingSpaces and countChar.
+3. Tried to stream input from text file. Unsucessful in current iteration.
+4. Does not contain unindent or indent functions.
+5. Functions work.
--- lab_08/funcs.cpp
+++ lab_08/funcs.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <cctype>
 
+//using a namespace
 using namespace std;
 
-
 string removeLeadingSpaces(string line){
+	
+	//counter
 	int i=0;
+
+	//while loop to find first !space
 	while(isspace(line[i])){
 		i++;
 	}
+
+	//returns line without leading spaces
 	return line.substr(i,line.length()-i);
 }
 
 int countChar(string line, char c){
+	
+	//counter
 	int o = 0;
+
+	//for loops to find matches to c, adding to o every time matched
 	for(int i=0; i<line.length(); i++){
 		if(line[i]==c){
 			o++;
@@ -22,6 +32,7 @@ int countChar(string line, char c){
 	return o;
 }
 
+//Trying to read from a file to test? I only inputted from the terminal
 /*
 int main(){
 	
